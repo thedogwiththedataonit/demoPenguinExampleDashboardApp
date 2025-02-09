6be4040c3ec29276d8c8124b01a0d3c410fb009f
Thomas Park <thomas0806.park@gmail.com>
2025-02-09 03:42:57 -0500

    reducing the recent-sales delay

--- src/app/dashboard/overview/@sales/page.tsx
+++ src/app/dashboard/overview/@sales/page.tsx
@@ -2,6 +2,6 @@ import { delay } from '@/constants/mock-api';
 import { RecentSales } from '@/features/overview/components/recent-sales';
 
 export default async function Sales() {
-  await delay(3000);
+  await delay(100);
   return <RecentSales />;
 }
